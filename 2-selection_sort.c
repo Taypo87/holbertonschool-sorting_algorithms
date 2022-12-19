d427b945009ce5a6be9fe1bb90f678050123c0a1
Taypo87 <taylor.poczynek87@gmail.com>
2022-12-19 14:36:21 -0800

    task 2 for sorting algorithms

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts a given list using selection sorting method
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0, index = 0;
+	int tmp = 0;
+
+	if (size < 2)
+		return;
+
+	while (i < size - 1)
+	{
+		index = i;
+		tmp = array[i];
+		j = i + 1;
+		while ( j < size)
+		{
+			if (array[j] < tmp)
+				tmp = array[j], index = j;
+			j++;
+		}
+		if (index != i)
+		{
+			array[idex] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+		i++;
+	}
+}
